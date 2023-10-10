e9bd3a9c9989a4265f5ab4394cea3dffa0956ba0
Andrew Kidd <contact@andrewiankidd.co.uk>
2023-10-10 19:12:45 +0100

    add view source button

--- src/js/script.js
+++ src/js/script.js
@@ -18,4 +18,11 @@ document.addEventListener("DOMContentLoaded", function(_event) {
             audio.play();
         });
     })
+
+    // view source button
+    document.body.innerHTML += `<div id="viewSource"><a id="viewSourceHyperlink">View Source On GitHub</a></div>`
+    const viewSourceButton = document.getElementById('viewSourceHyperlink');
+    const ghUser = window.location.host.split('.')[0];
+    const ghProject = window.location.pathname.split('/').filter(e => e)[0]
+    viewSourceButton.href = `https://github.com/${ghUser}/${ghProject}`
 });
